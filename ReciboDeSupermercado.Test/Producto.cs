1dafdf5df2332fc3f065702aae2f7505c4fbcde1
david.cubides <david.cubides@sinco.com.co>
2025-11-10 11:34:11 -0500

    🔴Si_AgregoElMismoProductoDosVeces_Debe_ExistirEnElReciboConCantidadDos

--- ReciboDeSupermercado.Test/Producto.cs
+++ ReciboDeSupermercado.Test/Producto.cs
@@ -4,4 +4,5 @@ public class Producto
 {
     public string Nombre { get; set; }
     public decimal Precio { get; set; }
+    public int Cantidad { get; set; }
 }
--- ReciboDeSupermercado.Test/ReciboDeSupermercadoTest.cs
+++ ReciboDeSupermercado.Test/ReciboDeSupermercadoTest.cs
@@ -26,12 +26,25 @@ public class ReciboDeSupermercadoTest
         
         recibo.Total.Should().Be(productosTestDatos.TotalEsperado);
     }
+
+    [Fact]
+    public void Si_AgregoElMismoProductoDosVeces_Debe_ExistirEnElReciboConCantidadDos()
+    {
+        var recibo = new Recibo();
+        
+        recibo.AgregarProducto("Cepillo de dientes", 0.99m);
+        recibo.AgregarProducto("Cepillo de dientes", 0.99m);
+        
+        recibo.Productos.Should().ContainSingle(p => p.Nombre == "Cepillo de dientes" && p.Cantidad == 2);
+    }
+    
 }
 
 public class Recibo
 {
     private decimal _total;
     public decimal Total => _total;
+    public List<Producto> Productos { get; set; }
 
     public void AgregarProducto(string productoDescripcion, decimal precio)
     {
