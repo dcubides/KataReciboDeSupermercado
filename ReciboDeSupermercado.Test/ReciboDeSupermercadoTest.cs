e7f29f4cf30bc7020b901a50e4574f6ae11993d8
david.cubides <david.cubides@sinco.com.co>
2025-11-10 10:19:24 -0500

    🔴Si_AgregarProductosAlReciboElTotal_Debe_SerLaSumaDeTodosLosPrecios

--- ReciboDeSupermercado.Test/ReciboDeSupermercadoTest.cs
+++ ReciboDeSupermercado.Test/ReciboDeSupermercadoTest.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections;
+using FluentAssertions;
 
 namespace ReciboDeSupermercado.Test;
 
@@ -44,6 +45,55 @@ public class ReciboDeSupermercadoTest
 
         recibo.Total.Should().Be(0.99m + 2.49m + 1.79m);
     }
+
+    [Fact]
+    [ClassData(typeof(DatosProductosTest))]
+    public void Si_AgregarProductosAlReciboElTotal_Debe_SerLaSumaDeTodosLosPrecios(ProductosTestDatos productosTestDatos)
+    {
+        var recibo = new Recibo();
+
+        foreach (var producto in productosTestDatos.Productos)
+        {
+            recibo.AgregarProducto(producto.Nombre, producto.Precio);
+        }
+        
+        recibo.Total.Should().Be(productosTestDatos.TotalEsperado);
+    }
+}
+
+public class DatosProductosTest : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new ProductosTestDatos
+            {
+                Productos = new List<Producto>
+                {
+                    new() { Nombre = "Cepillo de dientes", Precio = 0.99m }
+                },
+                TotalEsperado = 0.99m
+            }
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class ProductosTestDatos
+{
+    public List<Producto> Productos { get; set; } = new();
+    public decimal TotalEsperado { get; set; }
+}
+
+public class Producto
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
 }
 
 public class Recibo
